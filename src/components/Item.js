add11a461ddb146ec2d8ca6923f88de8bc45662d
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-22 22:12:12 -0500

    updated

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react"
 
 function Item({ name, category }) {
+    const [insideCart, cart] = useState(false)
+  // In the Item component, when the user clicks the <button> element, the item should be added to their virtual cart. The way we'll show the user that the item is in the cart is by changing the className on the <li> element:
+
+  function itemBtn(){
+      cart((insideCart) => !insideCart)
+  }
+
   return (
-    <li className="">
+    <li className= {insideCart ? "in-cart" : ""} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button  className={insideCart ? "reduce" : "add"} onClick={itemBtn}className="add">{insideCart ? "reduce" : "add"} Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,36 @@
-import React from "react";
 import Item from "./Item";
+import React, { useState } from "react";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  // console.log(items)
+  
+  // const foodList = selectedCategory.map((item) => {
+  //   return <li key={item.id} onClick={() => }>
+  // })
+
+
+  function handleFilterChange(event){
+    setSelectedCategory(event.target.value)
+    // console.log(event.target.value)
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if(selectedCategory === "All"){
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  });
+
+  // console.log(selectedCategory)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +38,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
